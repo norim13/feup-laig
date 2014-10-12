27132b659181273f00e2234cb56e993263745ccf
João Norim Bandeira <ei12022@fe.up.pt>
2014-10-12 18:03:49 +0000

    rectangulos agora lêm canto inf esq, e canto sup direito (acho eu)

--- PROJ1/CGFexample/include/Primitives.h
+++ PROJ1/CGFexample/include/Primitives.h
@@ -37,12 +37,12 @@ public:
 			glBegin(GL_QUADS);
 				glTexCoord2d(0,0);
 				glVertex3d(x1,y1,0);
-				glTexCoord2d(1,0);
-				glVertex3d(x1,y2,0);
-				glTexCoord2d(1,1);
-				glVertex3d(x2,y2,0);
 				glTexCoord2d(0,1);
 				glVertex3d(x2,y1,0);
+				glTexCoord2d(1,1);
+				glVertex3d(x2,y2,0);
+				glTexCoord2d(1,0);
+				glVertex3d(x1,y2,0);				
 			glEnd();
 
 		glPopMatrix();
