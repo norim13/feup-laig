a0a73edc05e1172f1e90937de1bafc18009437f2
ei12022 <ei12022@d9c5243e-d261-45e9-8914-329a4354473e>
2014-12-28 21:58:03 +0000

    prolog retorna peças adicionadas e removidas, e c++ já faz parsing disso

--- PROJ3/CGFexample/src/ParseProlog.h
+++ PROJ3/CGFexample/src/ParseProlog.h
@@ -4,7 +4,7 @@
 #include <string>
 #include "PieceData.h"
 
-std::vector<vector<PieceData> > parseBoard(std::string board_string);
+vector<vector<PieceData> > parseBoard(string board_string, vector<PieceData> &pecasAdicionadas, vector<PieceData> &pecasRemovidas);
 PieceData parsePiece(std::string piece);
 std::string boardToString(std::vector<std::vector<PieceData> > board);
 std::string jogadaToString(PieceData jogada, std::vector<vector<PieceData> > board);
@@ -16,6 +16,6 @@ std::string pieceToString(PieceData piece);
 
 std::string traduzirCoordenadas(int x, int y);
 
-bool parseAnswerJogada(std::string answer, vector<vector<PieceData> > &newBoard, string &gameOver);
+bool parseAnswerJogada(std::string answer, vector<vector<PieceData> > &newBoard, string &gameOver, vector<PieceData> &pecasAdicionadas, vector<PieceData> &pecasRemovidas);
 
 #endif
--- PROJ3/plogcon/socket.pl
+++ PROJ3/plogcon/socket.pl
@@ -24,7 +24,7 @@ serverLoop(Stream) :-
 	write('Received: '), write(ClientMsg), nl,
 	parse_input(ClientMsg, MyReply),
 	format(Stream, '~q.~n', [MyReply]),
-	/*write('Wrote: '), write(MyReply),*/ nl,
+	write('Wrote: '), write(MyReply), nl,
 	flush_output(Stream),
 	(ClientMsg == quit; ClientMsg == end_of_file), !.
 
@@ -32,19 +32,20 @@ parse_input(comando(Arg1, Arg2), Answer) :-
 	comando(Arg1, Arg2, Answer).
 	
 parse_input(quit, ok-bye) :- !.
-parse_input(novo-tabuleiro, [ok,Tabuleiro]) :- novoTabuleiro(7, Tabuleiro), /*tabuleiroExemplo1(Tabuleiro),*/ !.
+parse_input(novo-tabuleiro, [ok,Tabuleiro, [],[]]) :- novoTabuleiro(7, Tabuleiro), /*tabuleiroExemplo1(Tabuleiro),*/ !.
 
-parse_input([jogada, Jogada, Tabuleiro], [Msg,TabuleiroNovo]):- 
+parse_input([jogada, Jogada, Tabuleiro], [Msg,TabuleiroNovo,PecasAdicionadas, PecasRemovidas]):- 
 	(mesmaCorNaVizinhanca(Jogada, Tabuleiro); pecaSalto(Jogada)), 
 	colocaPeca(Jogada, Tabuleiro, T),
-	processaPecasEspeciais(T,TabuleiroNovo), write('deu'), nl,
+	processaPecasEspeciais(T,TabuleiroNovo, PecasAdicionadas, PecasRemovidas), write('deu'), nl,
 	end_game(Msg, TabuleiroNovo), !. 
 parse_input([jogada, Jogada, Tabuleiro], [not-ok,gg]):- write('n deu'), nl, !. /* celula estava ocupada, retorna not-ok */
 
 /*parse_input([jogada, Jogada, Tabuleiro], [ok,TabuleiroNovo]):- 
 	colocaPeca(Jogada, Tabuleiro, Tabuleiro2), write('deu'), nl,fazJogadaComputador('branca',Tabuleiro2,TabuleiroNovo), !. 
 */
-parse_input([computador,Cor,Tabuleiro], [Msg,TabuleiroNovo]) :- fazJogadaComputador(Cor,Tabuleiro,T),processaPecasEspeciais(T,TabuleiroNovo), end_game(Msg, TabuleiroNovo), !.
+parse_input([computador,Cor,Tabuleiro], [Msg,TabuleiroNovo,PecasAdicionadas,PecasRemovidas]) :- fazJogadaComputador(Cor,Tabuleiro,T), 
+	processaPecasEspeciais(T,TabuleiroNovo, PecasAdicionadas, PecasRemovidas), end_game(Msg, TabuleiroNovo), !.
 
 end_game(Msg, Tabuleiro):- (fimDoJogo(Tabuleiro, 0, 0, P, B),( (P>B, Msg = winPreto, nl); (B>P, Msg = winBranco, nl); (P =:= B, Msg = draw) );
 			Msg = ok).
