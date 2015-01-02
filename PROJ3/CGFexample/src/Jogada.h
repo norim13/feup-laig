028eac1ce6904f2de5e2899095fa07f6f7af04fe
João Norim Bandeira <ei12022@fe.up.pt>
2015-01-02 15:38:12 +0000

    jogador adicionado à Jogada. Cameras adicionadas à interface.

--- PROJ3/CGFexample/src/Jogada.h
+++ PROJ3/CGFexample/src/Jogada.h
@@ -9,9 +9,10 @@ private:
 	vector<PieceData> pecasAdicionadas;
 	vector<PieceData> pecasRemovidas;
 	static int nextId;
+	bool jogador; //branco -> true; preto -> false
 
 public:
-	Jogada(vector<PieceData> adicionadas, vector<PieceData> removidas);
+	Jogada(vector<PieceData> adicionadas, vector<PieceData> removidas, bool jogador);
 	int getId();
 	vector<PieceData> getAdicionadas();
 	vector<PieceData> getRemovidas();
